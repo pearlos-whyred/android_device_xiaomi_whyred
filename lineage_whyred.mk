10db69fd8df6683cbc58c283c334357f3a2c83cc
MyCats <entity499@yandex.by>
2019-06-18 15:49:13 +0300

    whyred: device makefile cleanup
    Don't override PixelExperience-specific vars

--- lineage_whyred.mk
+++ lineage_whyred.mk
@@ -24,8 +24,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 $(call inherit-product, device/xiaomi/whyred/device.mk)
 
 # Inherit some common LineageOS stuff.
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_whyred
